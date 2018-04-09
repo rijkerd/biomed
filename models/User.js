8ea255daf637298fdeaa2e9c0a0ac2048e2e6477
Ricardo Aggrey <rich2buzy@hotmail.com>
2018-04-09 08:48:32 +0300

    Refactored codebase
    deleted unnecessary files and added Profile and User models in the app.

--- models/User.js
+++ models/User.js
@@ -1,13 +1,33 @@
 const mongoose = require("mongoose");
+const bcrypt = require('bcrypt-nodejs');
+const crypto = require('crypto');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  googleId:String,
-  facebookId:String,
-  username:String,
-  email:String,
-  photo:String,
-  admin:Boolean
+  admissionNumber:{type:Number, unique:true, minlength:11},
+  password:{type:String,unique:true},
+  //approved:Boolean
 });
 
-mongoose.model("users",userSchema);
+
+// Hash the password before saving it to the database
+userSchema.pre('save',(next) => {
+  var user = this;
+  if(!user.isModified('password')) return next();
+  bcrypt.genSalt(10,(err,salt) => {
+    if(err) return next(err);
+    bcrypt.hash(user.password,salt,null,(err,hash) => {
+      if(err) return next(err);
+      user.password = hash;
+      next();
+    });
+  });
+});
+
+
+// Compare the password provided with the one in the database
+userSchema.methods.comparePassword = (password) => {
+  return bcrypt.compareSync(password,this.password);
+}
+
+module.exports = mongoose.model("user",userSchema);
--- webclient/src/reducers/authReducers.js
+++
@@ -1,6 +0,0 @@
-export default function(state = null, action) {
-  switch (action.type) {
-    default:
-      return state;
-  }
-}
