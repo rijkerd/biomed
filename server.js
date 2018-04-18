93034b8db69cef99d6af508f5cafe76f737dde0e
Ricardo Aggrey <rich2buzy@hotmail.com>
2018-04-18 15:13:06 +0300

    config for heroku deployment

--- server.js
+++ server.js
@@ -1,7 +1,6 @@
 const express = require("express");
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 5000;
 const mongoose = require("mongoose");
 const passport = require("passport");
 //const flash = require('connect-flash');
@@ -54,6 +53,6 @@ app.get("*",(req,res) => {
   res.sendFile(path.join(__dirname,'public/index.html'));
 })
 
-app.listen(PORT, () => {
+app.listen(keys.port, () => {
   console.log("App Listening on 5000");
 });
