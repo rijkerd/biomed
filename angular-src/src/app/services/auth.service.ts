4316394a7331c40db207938f868c466f867fa3b8
Ricardo Aggrey <rich2buzy@hotmail.com>
2018-04-21 22:52:59 +0300

    Ui making

--- angular-src/src/app/services/auth.service.ts
+++ angular-src/src/app/services/auth.service.ts
@@ -11,7 +11,7 @@ export class AuthService {
   isDev:boolean;
 
   constructor(private http: Http) {
-      this.isDev = true;  // Change to false before deployment
+      this.isDev = false;  // Change to false before deployment
       }
 
   registerUser(user) {
