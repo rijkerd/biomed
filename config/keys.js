f3da4185d47959224075367ab872d3b49819f9d3
rijkerd <rich2buzy@hotmail.com>
2018-12-13 13:34:13 +0300

    - Refactor of the codebase - Delete all files

--- config/keys.js
+++
@@ -1,7 +0,0 @@
-
-module.exports = {
-    database:process.env.MONDO_URI || 'mongodb://localhost:27017/classDB1',
-    secret:process.env.SECRET || 'jsdjfbsdf_DasdaASdsadmxzcndsufh',
-    port:process.env.PORT || 5000
-}
-
