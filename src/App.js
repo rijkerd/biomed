7144984ad763fc39e7d36e5e224cedfd6c52c8e0
rijkerd <richardaggrey7@gmail.com>
2020-04-01 16:08:29 +0300

    refactor: setup aws amplify

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useReducer, useContext, createContext } from "react";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+export const StateContext = createContext();
+
+console.log(StateContext);
+
+export const StateProvider = ({ reducer, initialState, children }) => (
+  <StateContext.Provider value={useReducer(reducer, initialState)}>
+    {children}
+  </StateContext.Provider>
+);
+
+export const useStateValue = () => useContext(StateContext);
+
+const App = () => <h1>Hello world</h1>;
 
 export default App;
