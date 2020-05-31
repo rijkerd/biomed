faa67349677a142a28e0f72a780d24fe5f045e22
rijkerd <richardaggrey7@gmail.com>
2020-06-01 00:23:51 +0300

    refactor: implement topic resource listing

--- module/urls.py
+++ module/urls.py
@@ -1,8 +1,14 @@
 from django.urls import path
-from .views import ModuleList, TopicList, TopicDetails
+from .views import ModuleList, TopicList, TopicDetails, TopicListJson
+from django.views.generic.base import TemplateView
+
+
+class TestTemplate(TemplateView):
+    template_name = "home.html"
 
 
 urlpatterns = [
+    path('', TestTemplate.as_view()),
     path('modules', ModuleList.as_view(), name='module-list'),
     path('topics', TopicList.as_view(), name='topic-list'),
     path('topics/<int:pk>', TopicDetails.as_view(), name='topic-detail'),
