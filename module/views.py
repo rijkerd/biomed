64eade6fbb14bae0fa955b145f835806cb03c93c
rijkerd <richardaggrey7@gmail.com>
2020-05-31 20:54:18 +0300

    refactor: add template files

--- module/views.py
+++ module/views.py
@@ -1,5 +1,8 @@
 from django.shortcuts import render
-from rest_framework.generics import ListCreateAPIView, RetrieveAPIView
+from rest_framework.generics import ListCreateAPIView, RetrieveAPIView, ListAPIView
+from rest_framework.renderers import TemplateHTMLRenderer
+from rest_framework.response import Response
+from rest_framework.views import APIView
 
 from .models import Module, Topic
 from .serializers import ModuleSerializer, TopicSerializer
@@ -10,9 +13,19 @@ class ModuleList(ListCreateAPIView):
     serializer_class = ModuleSerializer
 
 
-class TopicList(ListCreateAPIView):
+class TopicListJson(ListAPIView):
     queryset = Topic.objects.all()
     serializer_class = TopicSerializer
+
+
+class TopicList(APIView):
+    renderer_classes = [TemplateHTMLRenderer]
+    template_name = 'topic/show.html'
+
+    def get(self, request):
+        topic = Topic.objects.all()
+        serializer = TopicSerializer(topic, many=True)
+        return Response({'topics': serializer.data})
 
 
 class TopicDetails(RetrieveAPIView):
