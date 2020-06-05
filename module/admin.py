0a792b828363a1c20c498b8c00abbeb157ea49eb
rijkerd <richardaggrey7@gmail.com>
2020-06-05 20:48:03 +0300

    fix: rename admin panel title

--- module/admin.py
+++ module/admin.py
@@ -1,5 +1,8 @@
 from django.contrib import admin
 from .models import Module
 
+admin.site.site_header = 'Biomed Admin Panel'
+admin.site.site_title = 'Biomed'
+
 # Register your models here.
 admin.site.register(Module)
