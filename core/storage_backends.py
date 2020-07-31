1659236f44f072cc75200eca86f77cd8ec509e74
rijkerd <richardaggrey7@gmail.com>
2020-07-31 23:28:56 +0300

    refactor: configure s3 private storage

--- core/storage_backends.py
+++ core/storage_backends.py
@@ -1,12 +1,19 @@
+from django.conf import settings
 from storages.backends.s3boto3 import S3Boto3Storage
 
 
 class StaticStorage(S3Boto3Storage):
-    location = 'static'
-    default_acl = 'private'
+    location = settings.STATIC_LOCATION
+    # default_acl = 'private'
 
 
 class PublicMediaStorage(S3Boto3Storage):
-    location = 'media'
+    location = settings.PUBLIC_MEDIA_LOCATION
+    file_overwrite = False
+
+
+class PrivateMediaStorage(S3Boto3Storage):
+    location = settings.PRIVATE_MEDIA_LOCATION
     default_acl = 'private'
     file_overwrite = False
+    custom_domain = False
