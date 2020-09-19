d5559d33576a08307045189539a8fcf3800df2f1
rijkerd <richardaggrey7@gmail.com>
2020-09-19 16:57:42 +0300

    refactor: fix public access to resources

--- resources/models.py
+++ resources/models.py
@@ -1,6 +1,7 @@
 import uuid
 from django.db import models
 from topic.models import Topic
+from core.storage_backends import PublicMediaStorage
 
 
 class Resource(models.Model):
@@ -13,7 +14,7 @@ class Resource(models.Model):
     name = models.CharField(max_length=100, blank=False, default='default')
     description = models.CharField(
         max_length=200, blank=False, default='Not set')
-    location = models.FileField(upload_to='resources', blank=False)
+    location = models.FileField(storage=PublicMediaStorage(), blank=False)
     topic = models.ForeignKey(
         Topic, related_name='resources', on_delete=models.CASCADE)
     created_at = models.DateTimeField(auto_now_add=True)
