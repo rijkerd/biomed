75e9e7b141b54512472deb14f86c433921dedf69
rijkerd <richardaggrey7@gmail.com>
2022-07-13 10:29:02 +0300

    seed both teacher and student groups

---
+++ users/management/commands/seed_groups.py
@@ -0,0 +1,50 @@
+from unicodedata import name
+from django.core.management.base import BaseCommand
+from django.contrib.auth.models import Group,Permission
+from django.contrib.contenttypes.models import ContentType
+
+from topic.models import Topic
+from resources.models import Resource
+from module.models import Module
+
+
+class Command(BaseCommand):
+    help = 'Seed Groups for the System'
+
+    def handle(self, *args, **kwargs):
+        self.stdout.write('Seeding ....')
+        seed_teacher_group(self)
+        seed_student_group(self)
+        # seed_test(self)
+        self.stdout.write(self.style.SUCCESS('Seed Completed'))
+
+
+def seed_teacher_group(self):
+        teacher = Group.objects.filter(name="Teacher")
+        
+        if teacher.exists():
+            self.stdout.write('Teacher Group all ready exists')
+        else:
+            topics_content = ContentType.objects.get_for_model(Topic)
+            resource_content = ContentType.objects.get_for_model(Resource)
+            topic_permissions = Permission.objects.filter(content_type=topics_content)
+            resource_permissions = Permission.objects.filter(content_type=resource_content)
+            teacher_group,_ = Group.objects.get_or_create(name='Teacher')
+            teacher_group.permissions.set([*topic_permissions,*resource_permissions]);
+            self.stdout.write(self.style.SUCCESS('Seed Teacher Completed'))
+
+def seed_student_group(self):
+        student = Group.objects.filter(name="Student")
+        
+        if student.exists():
+            self.stdout.write('Student Group all ready exists')
+        else:
+            topics_content = ContentType.objects.get_for_model(Topic)
+            resource_content = ContentType.objects.get_for_model(Resource)
+            module_content =  ContentType.objects.get_for_model(Module)
+            topic_permissions = Permission.objects.get(content_type=topics_content,codename="view_topic")
+            resource_permissions = Permission.objects.get(content_type=resource_content,codename="view_resource")
+            module_permission = Permission.objects.get(content_type=module_content,codename="view_module")
+            student_group,_ = Group.objects.get_or_create(name='Student')
+            student_group.permissions.set([topic_permissions,resource_permissions,module_permission]);
+            self.stdout.write(self.style.SUCCESS('Seed Student Completed'))
